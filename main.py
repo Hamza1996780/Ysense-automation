23935d019fde9cbc3006e864d4faae9ef35ba871
Hamza Ahmed <32371294+Hamza1996780@users.noreply.github.com>
2023-05-01 00:15:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 with Ser() as bot:
     bot.land_first_page()
-    bot.login("hamza1996780@gmail.com","Hamza123")
+    bot.login("Email", "Password")
     bot.dailyserve()
     time.sleep(5000)
 
